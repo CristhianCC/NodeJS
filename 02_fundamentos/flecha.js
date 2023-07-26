65994c94a1ee8b1e282b4a0f257d105794f25d36
CristhianCC <cristhianmcm12@gmail.com>
2023-07-26 16:21:08 +0200

    [Node-Fund] Funciones de flecha

---
+++ 02_fundamentos/flecha.js
@@ -0,0 +1,16 @@
+// function sumar(a, b) {
+//     return a + b;
+// }
+
+// const sumar = (a, b = 10) => {
+//     return a + b;
+// }
+
+// const sumar = (a, b = 10) => {
+//     return a + b;
+// }
+
+const sumar = (a, b = 10) => a + b;
+const saludar = () => 'Hola Mundo';
+console.log(saludar());
+console.log(sumar(5))
