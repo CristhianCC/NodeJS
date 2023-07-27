c7d37304befe0879bdfe33a194f1d5290bb9e2b9
CristhianCC <cristhianmcm12@gmail.com>
2023-07-27 21:34:58 +0200

    [Node-Bases] Cómo importar archivos de nuestro proyecto

--- 03_bases_node/app.js
+++ 03_bases_node/app.js
@@ -1,22 +1,6 @@
-const fs = require('fs');
-let base = 3;
+const {crearArchivo} = require('./helpers/multiplicar');
 console.clear();
-
-console.log('====================');
-console.log(`   Tabla del: ${base}`);
-console.log('====================');
-
-let salida = '';
-
-for (let i = 1; i <= 10; i++) {
-    salida += `${base} x ${i} = ${base * i}\n`;
-}
-
-fs.writeFile(`tabla_${base}.txt`, salida, (err) => {
-    if (err) {
-        throw err;
-    }
-    console.log(`tabla_${base}.txt creada!`);
-});
-
-console.log(salida);
+let base = 2;
+crearArchivo(base)
+    .then(nombreArchivo => console.log(nombreArchivo, ' creado'))
+    .catch(err => console.log(err));
---
+++ 03_bases_node/helpers/multiplicar.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const crearArchivo = async(base = 5) => {
+    try {
+        console.log('====================');
+        console.log(`   Tabla del: ${base}`);
+        console.log('====================');
+
+        let salida = '';
+        for (let i = 1; i <= 10; i++) {
+            salida += `${base} x ${i} = ${base * i}\n`;
+        }
+
+        fs.writeFileSync(`tabla_${base}.txt`, salida);
+
+        return`tabla_${base}.txt`;
+    } catch (err) {
+        throw err;
+    }
+}
+
+module.exports = {
+    crearArchivo
+}
