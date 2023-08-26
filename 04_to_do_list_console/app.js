17389cee63a3eba67eec25b314da06bb1e96a03b
Cristhian <cristhianmcmc12@gmail.com>
2023-08-26 17:28:20 +0200

    [Node-ToDo-List] Las dos primeras opciones del menu creadas al igual que sus modelos

--- 04_to_do_list_console/app.js
+++ 04_to_do_list_console/app.js
@@ -1,15 +1,35 @@
 require('colors');
-const {inquirerMenu, pausa} = require('./helpers/inquirer');
+const {inquirerMenu, pausa, leerInput} = require('./helpers/inquirer');
+const Tareas = require( './models/tareas' );
 console.clear();
 
 const main = async () => {
-    console.log('Hola Mundo');
-
     let opt = '';
+
+    const tareas = new Tareas();
 
     do {
         opt = await inquirerMenu();
-        console.log({opt});
+
+        switch(opt) {
+            case '1':
+                const desc = await leerInput('Descripción: ');
+                tareas.crearTarea(desc);
+            break;
+            case '2':
+                console.log(tareas._listado)
+            break;
+            case '3':
+            break;
+            case '4':
+            break;
+            case '5':
+            break;
+            case '6':
+            break;
+            case '0':
+            break;
+        }
 
         await pausa();
     } while ( opt !== '0')
